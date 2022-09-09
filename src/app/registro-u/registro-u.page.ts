4044cd76ee546d9890794e04860a26da85f36a3c
Matias <101906720+Grizxz@users.noreply.github.com>
2022-09-09 17:27:54 -0300

    Update de funciones del formulario de registro-u

--- src/app/registro-u/registro-u.page.ts
+++ src/app/registro-u/registro-u.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms';
+import {AlertController} from '@ionic/angular'
 
 @Component({
   selector: 'app-registro-u',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-u.page.scss'],
 })
 export class RegistroUPage implements OnInit {
+  formularioRegistro: FormGroup;
+  
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) {
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'email': new FormControl("",Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required),
+      'carrera': new FormControl("", Validators.required),
+    });
+  }
+  ngOnInit() {}
+  async guardar(){
+    var f = this.formularioRegistro.value;
 
-  constructor() { }
-
-  ngOnInit() {
+    if(this.formularioRegistro.invalid){
+      const alerta = await this.alertController.create({
+        header :"Datos Faltantes",
+        message:"Debe rellenar todos los datos para completar el registro",
+        buttons:["OK"]
+      });
+    await alerta.present();
+    return;
+    }
+    var usuario = {
+      nombre: f.nombre,
+      email: f.email,
+      password: f.password,
+      carrera: f.carrera,
+      }
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+    console.log(usuario);
   }
 
 }

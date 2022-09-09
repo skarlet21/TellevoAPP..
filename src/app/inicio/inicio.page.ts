eb3951d59a74028fb7538ca4e65620b6cd80646e
skarlet21 <sk.contreras@duocuc.cl>
2022-09-09 16:58:49 -0300

    nuevo componente/ menu desplazable

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { IonModal } from '@ionic/angular';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+ @ViewChild(IonModal) modal: IonModal;
 
-  constructor() { }
 
+  constructor(private menuCtrl: MenuController) { }
+
+
+  onClick(){
+
+    this.menuCtrl.toggle();
+
+  }
+  cancel(){
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  
   ngOnInit() {
   }
 

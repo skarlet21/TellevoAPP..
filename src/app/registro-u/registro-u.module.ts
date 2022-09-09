c798e332361cdbe742cca6a59a8b23f8fe49ceee
Matias <101906720+Grizxz@users.noreply.github.com>
2022-09-09 19:02:05 -0300

    Ingreso de modulo faltante

--- src/app/registro-u/registro-u.module.ts
+++ src/app/registro-u/registro-u.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,7 @@ import { RegistroUPage } from './registro-u.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RegistroUPageRoutingModule
   ],
   declarations: [RegistroUPage]
